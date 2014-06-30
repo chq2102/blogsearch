62fe755774b93703843c7f4961a5b98f4d11a9be
chq2102 <chenqianalan@foxmail.com>
2014-06-30 11:22:30 +0800

    add some somment

--- src/bs/html2text/ParseHtmlToText.java
+++ src/bs/html2text/ParseHtmlToText.java
@@ -9,39 +9,45 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ParseHtmlToText {
+	//把需要查找的关键ID 放入一个数组
 	private static String idName[] = {"article_content","cnblogs_post_body",
 		"blog_content","sina_keyword_ad_area2"};
-
 	
+	//把解析后的内容输出到缓存中
 	public  StringBuffer ParserH(String htmlDrictoey) throws IOException
 	{
-		File input = new File(htmlDrictoey);
-		Document doc = Jsoup.parse(input, "UTF-8");
+		File input = new File(htmlDrictoey);//获取文件
+		Document doc = Jsoup.parse(input, "UTF-8");//用Jsoup解析文件生成docment
 		StringBuffer buffer = new StringBuffer();
 
-		Element blogContent = null;
+		Element blogContent = null;//要在文件中查找的元素
+		//按数组中的关键ID到文件中去查看是否有符合的元素
 		for(int i=0;i<idName.length;i++){
 
 			if(blogContent==null){
-				blogContent = doc.getElementById(idName[i]);
+				blogContent = doc.getElementById(idName[i]);//如果docment中有找到符合的元素，则提取给blogContent
 			}
 		}
+		//如果在文件中没有找到在数组中的关键ID 则去找特殊的class
 		if(blogContent==null){
 			blogContent = doc.getElementsByAttributeValue("class","Blog_wz1").first() ;
 		}
+		//blogContent不为空，即有找到元素
 		if(blogContent!=null){	
-			Elements tagDivs = blogContent.getElementsByTag("div");
+			Elements tagDivs = blogContent.getElementsByTag("div");//获取整个DIV中的元素
 
-			String title = doc.title();
-			String content = null;
+			String title = doc.title();//博客标题
+			String content = null;//博客内容
+			//逐个提取DIV元素中的内容 并添加到content中
 			for (Element link : tagDivs) {		  
 				String linkText = link.text();		 
 				content+=linkText;
 			} 
+			//把文件的标题和内容输出到buffer缓存中
 			buffer.append("标题："+title).append("\r\n");
-			buffer.append("内容："+content.replaceFirst("null", ""));
+			buffer.append("内容："+content.replaceFirst("null", ""));//由于DIV的原因 内容前面会有一个“null"字符串--去掉
 		}
-		
+		//不管有没有找到元素，返回buffer 该buffer有可能是空
 		return buffer;
 
 	}
--- src/bs/html2text/ToTextFile.java
+++ src/bs/html2text/ToTextFile.java
@@ -8,37 +8,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ToTextFile {
-	private static int i=0;
-	Matcher slashMatcher =null ;
-
+	private static int i=0;//文件的个数
+	//Matcher slashMatcher =null ;
+	//遍历获取指定路径下的所有文件
 	void getFiles(String filePath,String outputpath) throws IOException
 	{
 		File root = new File(filePath);
-		File[] files = root.listFiles();
-		for(File file:files)
+		File[] files = root.listFiles();//获取当前文件夹下所有子目录或文件， 并加入文件数组
+		for(File file:files)//(int n=0;n<files.length;n++)-->file = files[n];  
 		{     
-			if(file.isDirectory())
+			if(file.isDirectory())//判断files[n]是不是目录（不是文件）
 			{
-				getFiles(file.getAbsolutePath(),outputpath);
+				getFiles(file.getAbsolutePath(),outputpath);//递归遍历该目录
 			}
-			else
+			else//files[n]不是目录（是文件）
 			{
-				String fileName = file.getName();
+				String fileName = file.getName();//获得文件名
+				//判断文件是否是".html"文件
 				if((fileName.lastIndexOf(".")!=-1)&&fileName.substring(fileName.lastIndexOf(".")).equals(".html"))
-				{
+				{	//如果是".html"文件，获取该文件的绝对路径
 					String fileDir=file.getAbsolutePath();
-					int position = getCharacterPosition(fileDir, 6, "/");
+					int position = getCharacterPosition(fileDir, 6, "/");//在绝对路径中获取网址前一个分隔符位置
 					
 					BufferedWriter writer = null;
+					//字符串缓存
 					StringBuffer buffer = new StringBuffer();
+					//把该文件解析后的内容写入 buffer缓存中
 					buffer = new ParseHtmlToText().ParserH(file.getAbsolutePath());
+					//判断buffer长度，过滤空的buffer，避免大量空文件产生
 					if(buffer.length()>0){
+						//该文件的来源输出网址
 						System.out.println(file.getAbsolutePath().substring(position+1));
-						
+						//输出文件的路径和文件名(源文件的名字+i-->i避免同名文件被覆盖)、格式(.txt)
 						writer = new BufferedWriter(new FileWriter(outputpath+fileName.substring(0, fileName.lastIndexOf("."))+i+".txt"));
-						buffer.insert(0, "地址：http://"+file.getAbsolutePath().substring(position+1)+"\r\n") ;
-						writer.write(buffer.toString());
-						i++;
+						//在buffer  缓存最前面插入文件网址
+						buffer.insert(0, "地址：http://"+file.getAbsolutePath().substring(position+1)+"\r\n") ;	
+						writer.write(buffer.toString());//buffer 输出到txt文件
+						i++;//每生成一个文件 i+1，避免同名文件被覆盖
+						
+						writer.flush();//保证全部输出后关闭输出
 						writer.close();
 					}
 
@@ -49,9 +57,10 @@ public class ToTextFile {
 		}
 	}
 	
+	//定义一个获取 读取字符串第n次出现特定符号的位置的函数
 	public  int getCharacterPosition(String string ,int i,String character)
 	{  
-		slashMatcher = Pattern.compile("/").matcher(string);  
+		Matcher slashMatcher = Pattern.compile("/").matcher(string);  
 		int mIdx = 0;  
 		while(slashMatcher.find()) {  
 			mIdx++;  
